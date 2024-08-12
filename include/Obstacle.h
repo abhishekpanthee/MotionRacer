f285da1ad58b70956c72309b8b8b916ef91b6d1b
Sulav Paudel <151622439+Someone-tensai@users.noreply.github.com>
2024-08-12 22:50:23 +0545

    Update Obstacle.h

--- include/Obstacle.h
+++ include/Obstacle.h
@@ -3,11 +3,14 @@
 
 class Obstacle {
 private:
-    sf::Sprite sprite;
-    sf::Texture texture;
+    sf::Sprite obstacleSprite;
+    bool isOffScreen = false;
+    sf::Vector2u textureSize;
 
 public:
-    Obstacle(const std::string& textureFile, float x, float y);
+    Obstacle(sf::Texture &obstacleTexture);
+    void update(float deltaTime);
     void render(sf::RenderWindow& window) const; 
     sf::FloatRect getBounds() const;
+    bool isOffScreen() const;
 };
