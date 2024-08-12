ea01847b2f769e485dd37b36794e0ac301a1e27b
Sulav Paudel <151622439+Someone-tensai@users.noreply.github.com>
2024-08-12 22:44:08 +0545

    Update Obstacle.cpp

--- src/Obstacle.cpp
+++ src/Obstacle.cpp
@@ -1,22 +1,45 @@
 #include "Obstacle.h"
 #include <iostream>
 
-Obstacle::Obstacle(const std::string& textureFile, float x, float y) {
-    /// image ko directory texture file ma laijane  yo chai assocoate image chai .h file ma specify garexha
-    std::string fullPath = "assets/" + textureFile; 
-    
-    if (!texture.loadFromFile(fullPath)) {
-        std::cerr << "Failed to load image " << fullPath << std::endl;
+
+Obstacle::Obstacle(sf::Texture& obstacleTexture) {
+   
+    textureSize = obstacleTexture.getSize();
+    std::cout << textureSize.x;
+    obstacleSprite.setTexture(obstacleTexture);
+    obstacleSprite.rotate(270);
+    float x_position = static_cast<float>(rand() % (800 - 100)); // Random x within window width
+    float y_position = 300.f; // Start just above the screen
+    obstacleSprite.setPosition(x_position , y_position);
+    sf::Vector2f spriteSize(100.0f, 100.0f); // Assuming your sprite size should be 50x50
+    obstacleSprite.setScale(spriteSize.x / textureSize.x, spriteSize.y / textureSize.y);
+    obstacleTexture.setSmooth(true); // Optional: enables smoothing
+    obstacleTexture.setRepeated(false); // Ensure it's not being repeated
+}
+
+void Obstacle::update(float deltaTime)
+{
+    if (obstacleSprite.getPosition().y > 600)
+    {
+        isOffScreen = true;
     }
-    sprite.setTexture(texture);
-    sprite.setPosition(x, y);
+    obstacleSprite.move(0, 100 * deltaTime);
 }
 
 void Obstacle::render(sf::RenderWindow& window) const {
-    window.draw(sprite);
+ 
+    window.draw(obstacleSprite);
 }
 
-sf::FloatRect Obstacle::getBounds() const {
-   // tyo spr le gareko lai bounded garako 
-    return sprite.getGlobalBounds();
+sf::FloatRect Obstacle::getBounds() const
+{
+    return obstacleSprite.getGlobalBounds();
 }
+
+
+
+bool Obstacle::ifOffScreen() const
+{
+    return isOffScreen;
+}
+
