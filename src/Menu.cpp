764b519aa397cf5d7693c0eb73f37ba826356882
Sulav Paudel <151622439+Someone-tensai@users.noreply.github.com>
2024-08-12 22:56:59 +0545

    Update Menu.cpp

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -13,23 +13,27 @@ Menu::Menu()
 	menu[0].setFont(font);
 	menu[0].setFillColor(sf::Color::Red);
 	menu[0].setString("Start");
-	menu[0].setPosition(sf::Vector2f(width / 2, height / 5 * 1));
+	menu[0].setPosition(sf::Vector2f(width / 2, height / 6 * 1));
 
 	menu[1].setFont(font);
 	menu[1].setFillColor(sf::Color::White);
 	menu[1].setString("How to play");
-	menu[1].setPosition(sf::Vector2f(width / 2, height / 5 * 2));
+	menu[1].setPosition(sf::Vector2f(width / 2, height / 6 * 2));
 
 	menu[2].setFont(font);
 	menu[2].setFillColor(sf::Color::White);
 	menu[2].setString("Settings");
-	menu[2].setPosition(sf::Vector2f(width / 2, height / 5 * 3));
+	menu[2].setPosition(sf::Vector2f(width / 2, height / 6 * 3));
 
 	menu[3].setFont(font);
 	menu[3].setFillColor(sf::Color::White);
-	menu[3].setString("Exit");
-	menu[3].setPosition(sf::Vector2f(width / 2, height / 5 * 4));
+	menu[3].setString("High Score");
+	menu[3].setPosition(sf::Vector2f(width / 2, height / 6 * 4));
 
+	menu[4].setFont(font);
+	menu[4].setFillColor(sf::Color::White);
+	menu[4].setString("Exit");
+	menu[4].setPosition(sf::Vector2f(width / 2, height / 6 * 5));
 	selectedIndex = 0;
 }
 
@@ -47,37 +51,12 @@ bool Menu::updateMenu(sf::RenderWindow& window)
 			return true;
 		}
 		return false;
-
-	//sf::Event event;
-	//while (window.pollEvent(event))
-	//{
-	//	// check the type of the event...
-	//	switch (event.type)
-	//	{
-	//	case sf::Event::KeyPressed:
-	//		switch (event.key.code)
-	//		{
-	//		case sf::Keyboard::Up:
-	//			moveUp();
-	//			break;
-	//		case sf::Keyboard::Down:
-	//			moveDown();
-	//			break;
-	//		case sf::Keyboard::Enter:
-	//			return true;
-
-	//		default:
-	//			break;
-	//		}
-	//	}
-	//}
-	//return false;
 		
 }
 
 void Menu::drawMenu(sf::RenderWindow& window)
 {
-	for (int i = 0; i < 4; i++)
+	for (int i = 0; i < 5; i++)
 	{
 		window.draw(menu[i]);
 	}
@@ -95,7 +74,7 @@ void Menu::moveUp()
 
 void Menu::moveDown()
 {
-	if (selectedIndex + 1 < 4)
+	if (selectedIndex + 1 < 5)
 	{
 		menu[selectedIndex].setFillColor(sf::Color::White);
 		selectedIndex++;
