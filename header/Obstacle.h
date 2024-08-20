0002c01b2aa513f2c10b28b4058245c1ea4a7925
Sulav Paudel <151622439+Someone-tensai@users.noreply.github.com>
2024-08-20 16:11:52 +0545

    Get/Set position function

--- header/Obstacle.h
+++ header/Obstacle.h
@@ -6,11 +6,16 @@ private:
     sf::Sprite obstacleSprite;
     bool isOffScreen = false;
     sf::Vector2u textureSize;
+    float speedFactor;
 
 public:
     Obstacle(sf::Texture &obstacleTexture);
     void update(float deltaTime);
     void render(sf::RenderWindow& window) const; 
+
+    sf::Vector2f getPosition() const;
+    void setPosition(sf::Vector2f);
+
     sf::FloatRect getBounds() const;
     bool ifOffScreen() const;
 };
