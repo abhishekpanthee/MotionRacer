7243e50f0e6a6c1a26d44e5e9599bd52baeb5f64
Sulav Paudel <151622439+Someone-tensai@users.noreply.github.com>
2024-08-20 16:12:58 +0545

    Get/Set position function

--- header/PowerUp.h
+++ header/PowerUp.h
@@ -4,14 +4,18 @@
 class PowerUp {
 private:
     sf::Sprite sprite;
-    bool isOffScreen;
     sf::Vector2u textureSize;
+    float speedFactor;
 
 public:
     PowerUp();
     PowerUp(sf::Texture& powerUpTexture);
     void render(sf::RenderWindow& window) const; 
     bool update(float deltaTime);
+
+    sf::Vector2f getPosition() const;
+    void setPosition(sf::Vector2f);
+
     sf::FloatRect getBounds() const;
 
 };
