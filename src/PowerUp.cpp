e4032a58d827a9b38fb033153870f622a1db2c6b
Sulav Paudel <151622439+Someone-tensai@users.noreply.github.com>
2024-08-20 16:33:34 +0545

    Update PowerUp.cpp

--- src/PowerUp.cpp
+++ src/PowerUp.cpp
@@ -1,26 +1,18 @@
 #include "../header/PowerUp.h"
 #include <iostream>
 
-PowerUp::PowerUp(){}
-PowerUp::PowerUp(sf::Texture& powerUpTexture):isOffScreen{false} {
-    //  image ko directory texture file ma laijane path contrsut gareko 
-
-    sprite.setTexture(powerUpTexture);
+PowerUp::PowerUp() : speedFactor {200.0f} {}
+PowerUp::PowerUp(sf::Texture& powerUpTexture): speedFactor {200.0f} {
+    
     textureSize = powerUpTexture.getSize();
-    std::cout << textureSize.x;
     sprite.setTexture(powerUpTexture);
     sprite.rotate(270);
-    float x_position = static_cast<float>(rand() % (800 - 100)); // Random x within window width
-    float y_position = 300.f; 
-    sprite.setPosition(x_position, y_position);
     sf::Vector2f spriteSize(50.0f, 50.0f); 
     sprite.setScale(spriteSize.x / textureSize.x, spriteSize.y / textureSize.y);
     powerUpTexture.setSmooth(true); // Optional: enables smoothing
     powerUpTexture.setRepeated(false); // Ensure it's not being repeated    
-    
-
-
 }
+
 void PowerUp::render(sf::RenderWindow& window) const {
     
     window.draw(sprite);
@@ -29,15 +21,24 @@ void PowerUp::render(sf::RenderWindow& window) const {
 
 bool PowerUp::update(float deltaTime)
 {
-    if (sprite.getPosition().y > 600)
+    if (sprite.getPosition().y > 700)
     {
         return true;  // Indicate that the PowerUp should be deleted
     }
 
-    sprite.move(0, 100 * deltaTime);
+    sprite.move(0 , speedFactor * deltaTime);
     return false;  // Indicate that the PowerUp should not be deleted
 }
 
+sf::Vector2f PowerUp::getPosition() const
+{
+    return this->sprite.getPosition();
+}
+
+void PowerUp::setPosition(sf::Vector2f position)
+{
+    this->sprite.setPosition(position);
+}
 
 sf::FloatRect PowerUp::getBounds() const {
     // tyo sprite le gareko lai bounded garako 
