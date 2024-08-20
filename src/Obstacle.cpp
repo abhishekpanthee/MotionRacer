38ec1d0b8d4f08289a21945b1545d718c7759677
Sulav Paudel <151622439+Someone-tensai@users.noreply.github.com>
2024-08-20 16:31:09 +0545

    Update Obstacle.cpp

--- src/Obstacle.cpp
+++ src/Obstacle.cpp
@@ -2,15 +2,11 @@
 #include <iostream>
 
 
-Obstacle::Obstacle(sf::Texture& obstacleTexture) {
+Obstacle::Obstacle(sf::Texture& obstacleTexture): speedFactor{200.0f} {
    
     textureSize = obstacleTexture.getSize();
-    std::cout << textureSize.x;
     obstacleSprite.setTexture(obstacleTexture);
     obstacleSprite.rotate(270);
-    float x_position = static_cast<float>(rand() % (800 - 100)); // Random x within window width
-    float y_position = 300.f; 
-    obstacleSprite.setPosition(x_position , y_position);
     sf::Vector2f spriteSize(100.0f, 100.0f); 
     obstacleSprite.setScale(spriteSize.x / textureSize.x, spriteSize.y / textureSize.y);
     obstacleTexture.setSmooth(true); // Optional: enables smoothing
@@ -19,24 +15,43 @@ Obstacle::Obstacle(sf::Texture& obstacleTexture) {
 
 void Obstacle::update(float deltaTime)
 {
-    if (obstacleSprite.getPosition().y > 600)
+    if (obstacleSprite.getPosition().y > 700)
     {
         isOffScreen = true;
     }
-    obstacleSprite.move(0, 100 * deltaTime);
+    obstacleSprite.move(0, speedFactor * deltaTime);
+    speedFactor += 10 * deltaTime;
 }
 
 void Obstacle::render(sf::RenderWindow& window) const {
- 
-    window.draw(obstacleSprite);
+
+    //A red rectangle in the obstacle to show its bounds as collision was happening without touching the actual image
+   sf::FloatRect carBounds = obstacleSprite.getGlobalBounds();
+   sf::RectangleShape rect(obstacleSprite.getPosition());
+   rect.setSize(sf::Vector2f(carBounds.width, carBounds.height));
+   rect.setPosition(carBounds.left, carBounds.top);
+   rect.setOutlineThickness(5.0f);
+   rect.setOutlineColor(sf::Color::Red);
+   rect.setFillColor(sf::Color::Transparent);
+   window.draw(rect);
+   
+   window.draw(obstacleSprite);
+}
+
+sf::Vector2f Obstacle::getPosition() const
+{
+    return this->obstacleSprite.getPosition();
+}
+
+void Obstacle::setPosition(sf::Vector2f position)
+{
+    this->obstacleSprite.setPosition(position);
 }
 
 sf::FloatRect Obstacle::getBounds() const
 {
     return obstacleSprite.getGlobalBounds();
 }
-
-
 
 bool Obstacle::ifOffScreen() const
 {
